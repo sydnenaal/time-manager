eeae5e6594d78370ee005b40275d5baf29934324
sydnenaal <dimasymbain@gmail.com>
2021-07-11 23:51:42 +0300

    fix test

--- frontend/src/Components/Atoms/Button/Button.tsx
+++ frontend/src/Components/Atoms/Button/Button.tsx
@@ -1,12 +1,11 @@
-import { memo, ReactNode } from "react"
+import { memo, ReactNode } from 'react'
 
-import { StyledButton } from "./Button.styles";
+import { StyledButton } from './Button.styles'
 
 interface IButtonProps {
     children: ReactNode
 }
 
-export const Button = memo(({ children }: IButtonProps)  => {
+export const Button = memo(({ children }: IButtonProps) => {
     return <StyledButton>{children}</StyledButton>
-});
-
+})

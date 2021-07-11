cccccf34e146cdf1207d259330a61534d511855e
sydnenaal <dimasymbain@gmail.com>
2021-07-12 00:27:29 +0300

    add test environment

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
     setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
     testPathIgnorePatterns: ['/node_modules/', '/public/'],
     testMatch: ['<rootDir>/src/**/*.spec.(ts|tsx)'],
+    testEnvironment: 'jsdom',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 }

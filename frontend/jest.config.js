6046a2ba6f7b8f18f1890592293b912f436b3719
fr13n8 <ovsepanrik@gmail.com>
2021-07-15 20:08:45 +0300

    FIX ALL ERRORS AND BUGS

--- frontend/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    roots: ['<rootDir>/src'],
-    transform: { '^.+\\.tsx?$': 'ts-jest' },
-    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-    testPathIgnorePatterns: ['/node_modules/', '/public/'],
-    testMatch: ['<rootDir>/src/**/*.spec.(ts|tsx)'],
-    testEnvironment: 'jsdom',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-}

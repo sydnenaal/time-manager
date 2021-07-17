182f8eb85568d2c5e2abe4c6f6e2e0a16500410d
Moroyzik <white_bear1902@mail.ru>
2021-07-17 22:06:17 +0300

    added index for AuthForm

--- frontend/src/Components/Molecules/AuthForm/__tests__/AuthForm.spec.tsx
+++ frontend/src/Components/Molecules/AuthForm/__tests__/AuthForm.spec.tsx
@@ -1,24 +1,10 @@
 import { render } from '@testing-library/react'
-import { unmountComponentAtNode } from 'react-dom'
+
 import { Button } from '../../../Atoms/Button/Button'
+
 import { AuthForm } from '../AuthForm'
 
 describe('<AuthForm />', () => {
-    let container: HTMLDivElement | null = null
-
-    beforeEach(() => {
-        container = document.createElement('div')
-        document.body.appendChild(container)
-    })
-
-    afterEach(() => {
-        if (container) {
-            unmountComponentAtNode(container)
-            container.remove()
-        }
-        container = null
-    })
-
     it('should renders correctly', () => {
         const tree = render(
             <AuthForm children={<Button children="simple button" onClick={() => null} />} />
